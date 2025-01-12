836d1e46c58d9ef96919b3e42589fa8c70a9ccd4
JavierTomasTormo <106741000+JavierTomasTormo@users.noreply.github.com>
2025-01-12 22:37:10 +0100

    Update index.spec.js

--- cypress/integration/1-my-tests/pages/index.spec.js
+++ cypress/integration/1-my-tests/pages/index.spec.js
@@ -5,6 +5,6 @@ let TOTAL_USERS = 10;
 describe("Test de endpoints", () => {
   it("Endpoint to check index title", () => {
     cy.visit("/");
-    cy.title().should("eq", "My example app");
+    cy.title().should("eq", "Javier Tomás Tormo - Portfolio");
   });
 });
